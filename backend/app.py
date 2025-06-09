46ea0a9edc9036fd534e308687533233ad809474
hungkaihsin <kaihsin.hung0802@gmail.com>
2025-06-09 13:01:07 +0800

    feat: build frontend

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,11 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
-from src.api.models.prediction import lstm_prediction, gru_prediction, conv1d_prediction, ffn_prediction
-from src.api.models.tools.plot import plot
+app = Flask(__name__)
+CORS(app)
 
-# plot(lstm_prediction())
-plot(lstm_prediction())
-plot(ffn_prediction())
-plot(gru_prediction())
-plot(conv1d_prediction())
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
