a30f5eb9e4e859ba66c45ec8234eda44ba1d43d3
hungkaihsin <kaihsin.hung0802@gmail.com>
2025-06-12 17:07:23 +0800

    feat: add animation

--- frontend/src/pages/IntroductionPage.jsx
+++ frontend/src/pages/IntroductionPage.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import './IntroductionPage.css'
 
+
 export default function IntroductionPage() {
   const navigate = useNavigate()
   return (
