eec35b97b35ee601d6faa9102f7821fad36142f2
hungkaihsin <k_hung2@u.pacific.edu>
2025-07-09 15:31:37 +0800

    fix: reduce memory useage

--- backend/src/api/models/tools/tuner.py
+++ backend/src/api/models/tools/tuner.py
@@ -1,136 +1,33 @@
 from src.api.models.model import lstm_model, gru_model, conv1d_model, ffn_model
-from src.api.models.preprocess import preprocess_dataset 
-from src.api.models.tools.general import get_filepath
-from keras.callbacks import EarlyStopping
 from keras_tuner import RandomSearch
-
-
-
-
+import os
 
 log_dir_path = 'src/dataset/tuning'
 
-def early_stop():
-    early_stop = EarlyStopping(patience=10, restore_best_weights=True)
-
-    return early_stop
-
-
-def lstm_tuner(log_dir=log_dir_path, project_name='lstm'):
-    
-    # Can change this to allow user choose another file
-
-    data = preprocess_dataset(get_filepath())
-
-    X_train, y_train = data['X_train'], data['y_train']
-    X_val, y_val = data['X_val'], data['y_val']
+def load_best_model(project_name):
+    """
+    Loads the best model from a Keras Tuner project.
+    """
+    model_builder = None
+    if project_name == 'lstm':
+        model_builder = lstm_model
+    elif project_name == 'gru':
+        model_builder = gru_model
+    elif project_name == 'conv1d':
+        model_builder = conv1d_model
+    elif project_name == 'ffn_model':
+        model_builder = ffn_model
 
     tuner = RandomSearch(
-        lstm_model,
+        model_builder,
         objective='val_mae',
         max_trials=30,
         executions_per_trial=2,
-        directory=log_dir,
+        directory=log_dir_path,
         project_name=project_name
     )
 
-    tuner.search(X_train, y_train, validation_data=(X_val, y_val), epochs=100, batch_size=32, callbacks=[early_stop()])
-
-
-
+    # The tuner needs to be reloaded to get the best model
+    tuner.reload()
     best_model = tuner.get_best_models(num_models=1)[0]
-
-    return best_model
-
-
-
-
-def gru_tuner(log_dir=log_dir_path, project_name='gru'):
-    
-    # Can change this to allow user choose another file
-
-    data = preprocess_dataset(get_filepath())
-
-    X_train, y_train = data['X_train'], data['y_train']
-    X_val, y_val = data['X_val'], data['y_val']
-
-    tuner = RandomSearch(
-        gru_model,
-        objective='val_mae',
-        max_trials=30,
-        executions_per_trial=2,
-        directory=log_dir,
-        project_name=project_name
-    )
-
-    tuner.search(X_train, y_train, validation_data=(X_val, y_val), epochs=100, batch_size=32, callbacks=[early_stop()])
-
-
-
-    best_model = tuner.get_best_models(num_models=1)[0]
-
-    return best_model
-
-
-
-def conv1d_tuner(log_dir=log_dir_path, project_name='conv1d'):
-    
-    # Can change this to allow user choose another file
-
-    data = preprocess_dataset(get_filepath())
-
-    X_train, y_train = data['X_train'], data['y_train']
-    X_val, y_val = data['X_val'], data['y_val']
-
-    tuner = RandomSearch(
-        conv1d_model,
-        objective='val_mae',
-        max_trials=30,
-        executions_per_trial=2,
-        directory=log_dir,
-        project_name=project_name
-    )
-
-    tuner.search(X_train, y_train, validation_data=(X_val, y_val), epochs=100, batch_size=32, callbacks=[early_stop()])
-
-
-
-    best_model = tuner.get_best_models(num_models=1)[0]
-
-    return best_model
-
-
-
-
-
-def ffn_model_tuner(log_dir=log_dir_path, project_name='ffn_model'):
-    
-    # Can change this to allow user choose another file
-
-    data = preprocess_dataset(get_filepath())
-
-    X_train, y_train = data['X_train'], data['y_train']
-    X_val, y_val = data['X_val'], data['y_val']
-
-
-    # reshape
-
-    X_train_ff = X_train.reshape(X_train.shape[0], -1)
-    X_val_ff = X_val.reshape(X_val.shape[0], -1)
-
-    tuner = RandomSearch(
-        ffn_model,
-        objective='val_mae',
-        max_trials=30,
-        executions_per_trial=2,
-        directory=log_dir,
-        project_name=project_name
-    )
-
-    tuner.search(X_train_ff, y_train, validation_data=(X_val_ff, y_val), epochs=100, batch_size=32, callbacks=[early_stop()])
-
-
-
-    best_model = tuner.get_best_models(num_models=1)[0]
-
     return best_model
