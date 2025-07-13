8cfa4f68fddd3a9fab677f013bbffb4b00a588bf
hungkaihsin <kaihsin.hung0802@gmail.com>
2025-07-13 11:25:42 +0800

    fix: use Hash rotuer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
 // src/App.jsx
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom'
 import PredictionPage from './pages/PredictionPage.jsx'
 import IntroductionPage from './pages/IntroductionPage.jsx'
 import './App.css'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         {/* Redirect root to /predict */}
         <Route path="/" element={<Navigate to="/intro" replace />} />
@@ -14,7 +14,7 @@ function App() {
         <Route path="/intro" element={<IntroductionPage />} />
         {/* later you can add more pages here */}
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
